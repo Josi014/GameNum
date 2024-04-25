06013da7b118b4286e17bf3f79a6b709ddbc700d
Josiéli Souza Ramos <josielisouzaderamos714@gmail.com>
2024-04-25 19:38:41 -0300

    Melhoria no código

--- SortNumber.java
+++ SortNumber.java
@@ -11,52 +11,99 @@ public class SortNumber {
         int ten = 0;
 
         while (errou) {
-            ten=0;
-
+            ten = 0;
             Random gerador = new Random();
             for (int v = 0; v < 3; v++) {
                 numSort[v] = gerador.nextInt(10);
             }
             for (int j = 0; j < numSort.length; j++) {
+                String fraseSorteada = "";
                 for (int i = 0; i < 1; i++) {
-                    if (numSort[j] == 0) {
-                        System.out.println("O número de três digitos sorteado possui algarismos com a letra Z");
-                    }
-                    if (numSort[j] == 1) {
-                        System.out.println("O número de três digitos sorteado possui algarismos com a letra U ");
-                    }
-                    if (numSort[j] == 2) {
-                        System.out.println("O número de três digitos sorteado possui algarismos com a letra D");
-                    }
-                    if (numSort[j] == 3) {
-                        System.out.println("O número de três digitos sorteado possui algarismos com a letra T");
-                    }
-                    if (numSort[j] == 4) {
-                        System.out.println("O número de três digitos sorteado possui algarismos com a letra Q");
-                    }
-                    if (numSort[j] == 5) {
-                        System.out.println("O número de três digitos sorteado possui algarismos com a letra C");
-                    }
-                    if (numSort[j] == 6) {
-                        System.out.println("O número de três digitos sorteado possui algarismos com a letra S");
-                    }
-                    if (numSort[j] == 7) {
-                        System.out.println("O número de três digitos sorteado possui algarismos com as letras S e T");
-                    }
-                    if (numSort[j] == 8) {
-                        System.out.println("O número de três digitos sorteado possui algarismos com a letra O");
-                    }
-                    if (numSort[j] == 9) {
-                        System.out.println("O número de três digitos sorteado possui algarismos com a letra N");
-                    }
-                }
 
+                    String[] frases = {
+                            "O número de três digitos sorteado possui um algarismo com a letra Z",
+                            "O número de três digitos sorteado possui um algarismo entre -1 e 1",
+                            "O número de três digitos sorteado possui um algarismo com a letra U",
+                            "O número de três digitos sorteado possui um algarismo entre 0 e 2",
+                            "O número de três digitos sorteado possui um algarismo com a letra D",
+                            "O número de três digitos sorteado possui um algarismo entre 1 e 3",
+                            "O número de três digitos sorteado possui um algarismo com a letra T",
+                            "O número de três digitos sorteado possui um algarismo entre 2 e 4",
+                            "O número de três digitos sorteado possui um algarismo com a letra Q",
+                            "O número de três digitos sorteado possui um algarismo entre 3 e 5",
+                            "O número de três digitos sorteado possui um algarismo com a letra C",
+                            "O número de três digitos sorteado possui um algarismo entre 4 e 6",
+                            "O número de três digitos sorteado possui um algarismo com a letra S",
+                            "O número de três digitos sorteado possui um algarismo entre 5 e 7",
+                            "O número de três digitos sorteado possui um algarismo com as letras S e T",
+                            "O número de três digitos sorteado possui um algarismo entre 6 e 8",
+                            "O número de três digitos sorteado possui um algarismo com a letra O",
+                            "O número de três digitos sorteado possui um algarismo entre 7 e 9",
+                            "O número de três digitos sorteado possui um algarismo com a letra N",
+                            "O número de três digitos sorteado possui um algarismo entre 8 e 10"
+
+                    };
+
+                    if (numSort[j] == 0 && ten == 0) {
+                        fraseSorteada = frases[0];
+                    } else if (numSort[j] == 0 && ten == 1) {
+                        fraseSorteada = frases[1];
+                        ten = 2;
+                    } else if (numSort[j] == 1 && ten == 0) {
+                        fraseSorteada = frases[2];
+                    } else if (numSort[j] == 1 && ten == 1) {
+                        fraseSorteada = frases[3];
+                        ten = 2;
+                    } else if (numSort[j] == 2 && ten == 0) {
+                        fraseSorteada = frases[4];
+                    } else if (numSort[j] == 2 && ten == 1) {
+                        fraseSorteada = frases[5];
+                        ten = 2;
+                    } else if (numSort[j] == 3 && ten == 0) {
+                        fraseSorteada = frases[6];
+                    } else if (numSort[j] == 3 && ten == 1) {
+                        fraseSorteada = frases[7];
+                        ten = 2;
+                    } else if (numSort[j] == 4 && ten == 0) {
+                        fraseSorteada = frases[8];
+                    } else if (numSort[j] == 4 && ten == 1) {
+                        fraseSorteada = frases[9];
+                        ten = 2;
+                    } else if (numSort[j] == 5 && ten == 0) {
+                        fraseSorteada = frases[10];
+                    } else if (numSort[j] == 5 && ten == 1) {
+                        fraseSorteada = frases[11];
+                        ten = 2;
+                    } else if (numSort[j] == 6 && ten == 0) {
+                        fraseSorteada = frases[12];
+                    } else if (numSort[j] == 6 && ten == 1) {
+                        fraseSorteada = frases[13];
+                        ten = 2;
+                    } else if (numSort[j] == 7 && ten == 0) {
+                        fraseSorteada = frases[14];
+                    } else if (numSort[j] == 7 && ten == 1) {
+                        fraseSorteada = frases[15];
+                        ten = 2;
+                    } else if (numSort[j] == 8 && ten == 0) {
+                        fraseSorteada = frases[16];
+                    } else if (numSort[j] == 8 && ten == 1) {
+                        fraseSorteada = frases[17];
+                        ten = 2;
+                    } else if (numSort[j] == 9 && ten == 0) {
+                        fraseSorteada = frases[18];
+                    } else if (numSort[j] == 9 && ten == 1) {
+                        fraseSorteada = frases[19];
+                        ten = 2;
+                    }
+
+                }
+                System.out.println(fraseSorteada);
                 System.out.println("Dê seu chute");
                 digite[j] = sc.nextInt();
-                if (digite[j] != numSort[j]) {
+                if (digite[j] != numSort[j]) { 
                     System.out.println("Ops, tente novamente");
                     j--;
-                    if (ten == 1) {
+                    if (ten == 2) {
                         break;
                     }
                     ten = 1;
@@ -67,13 +114,18 @@ public class SortNumber {
             }
 
             if (digite[0] == numSort[0] && digite[1] == numSort[1] && digite[2] == numSort[2]) {
-                System.out.println("Você acertou!Parabéns :)");
+                System.out.println("Você acertou,o número sorteado era: ");
+                for (int ler = 0; ler < 3; ler++) {
+                    System.out.println(numSort[ler]);
+                }
+                System.out.println("!Parabéns :)");
                 errou = false;
             } else {
                 System.out.println("Não foi desta vez :(");
 
             }
         }
+        sc.close();
     }
 
 }
